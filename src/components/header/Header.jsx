79f37b71b811ac9767341424b0d45ff5e61eb182
nejmimouhsine <nejmi.mouhcine10@gmail.com>
2020-12-07 15:14:33 +0100

    Install creative agency react app and node-sass and create the header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <div className='logo'>
+                <h2>Creative</h2>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
